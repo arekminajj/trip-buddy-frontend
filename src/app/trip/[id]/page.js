c4ac6caa82f9cb61a6dd6ad5fb2908c413d0ac82
Arkadiusz Cios <arek123113@gmail.com>
2025-05-21 01:10:03 +0200

    feat: #dev integrate google maps with the details page

--- src/app/trip/[id]/page.js
+++ src/app/trip/[id]/page.js
@@ -142,6 +142,16 @@ export default async function TripDetailPage({ params }) {
           </p>
         </div>
 
+        <iframe
+          width={600}
+          height={450}
+          style={{ border: 0 }}
+          loading="lazy"
+          allowFullScreen
+          referrerPolicy="no-referrer-when-downgrade"
+          src={"https://www.google.com/maps/embed/v1/place?key=" + process.env.MAPS_API_KEY +"&q=" + trip.endLocation}
+        ></iframe>
+
         <div
           style={{
             marginTop: "25px",
