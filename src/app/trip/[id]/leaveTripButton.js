359963f44984433a1fd49065319f239afdaff9a4
Arkadiusz Cios <arek123113@gmail.com>
2025-05-20 14:53:53 +0200

    fix: change method to POST on leave button

--- src/app/trip/[id]/leaveTripButton.js
+++ src/app/trip/[id]/leaveTripButton.js
@@ -26,7 +26,7 @@ export default function LeaveTripButton({ tripId }) {
       const res = await fetch(
         `${process.env.NEXT_PUBLIC_BASE_URL}/api/trip/${tripId}/leave`,
         {
-          method: "DELETE",
+          method: "POST",
           headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${session.accessToken}`,
